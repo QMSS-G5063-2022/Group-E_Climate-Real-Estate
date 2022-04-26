59b9bb67e7d152d20f78eb83b51d822e5357e71a
Cindy Chen <chencindyj@gmail.com>
2022-04-25 21:05:24 -0400

    add more data (bottom tier) and update UI file

--- Shiny Draft/server.R
+++ Shiny Draft/server.R
@@ -5,4 +5,85 @@ library(dplyr)
 
 # Leaflet bindings are a bit slow; for now we'll just sample to compensate
 set.seed(100)
-#zipdata <- read.csv("~\..\data\buffalo.csv")
+dataset1 <- read.csv("~\..\data\buffalo.csv")
+dataset2 <- read.csv("~\..\data\buffalo.csv")
+
+function(input, output, session){
+  
+  ### NEW ORLEANS ###
+  
+  town <- merge(dataset1, dataset2, by='zipcode',duplicateGeoms=TRUE)
+  
+  bins <- bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf) 
+
+  pal1 <- colorBin("YlOrRd",domain = town$id, bins=bins)
+  
+  color_id1 <- pal1(town$id)
+  
+  content1 <- paste("Neighborhood:", town$neighbourhood,"<br/>") #Popup content to annotate the neighborhoods. 
+  
+  townmap <- leaflet(town) %>% setView(-0, 0, zoom = 10) %>% 
+    addPolygons(stroke=TRUE, smoothFactor = 0.5, weight=1, color=color_id1,popup=content1, opacity=1, fillColor=~colorQuantile('YlOrRd', town$id)(town$id),fillOpacity=1) %>% 
+    addProviderTiles("Stamen.TonerLite") %>% 
+    addLegend(pal=pal1, values= ~town$id, title = "Zipcode Mean of Real Estate Price")
+
+  ### COFFEY PARK ###
+
+town <- merge(dataset1, dataset2, by='zipcode',duplicateGeoms=TRUE)
+bins <- bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf) 
+pal1 <- colorBin("YlOrRd",domain = town$id, bins=bins)
+color_id1 <- pal1(town$id)
+content1 <- paste("Neighborhood:", town$neighbourhood,"<br/>") #Popup content to annotate the neighborhoods. 
+townmap <- leaflet(town) %>% setView(-0, 0, zoom = 10) %>% 
+  addPolygons(stroke=TRUE, smoothFactor = 0.5, weight=1, color=color_id1,popup=content1, opacity=1, fillColor=~colorQuantile('YlOrRd', town$id)(town$id),fillOpacity=1) %>% 
+  addProviderTiles("Stamen.TonerLite") %>% 
+  addLegend(pal=pal1, values= ~town$id, title = "Zipcode Mean of Real Estate Price")
+
+### DENVER ###
+
+town <- merge(dataset1, dataset2, by='zipcode',duplicateGeoms=TRUE)
+bins <- bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf) 
+pal1 <- colorBin("YlOrRd",domain = town$id, bins=bins)
+color_id1 <- pal1(town$id)
+content1 <- paste("Neighborhood:", town$neighbourhood,"<br/>") #Popup content to annotate the neighborhoods. 
+townmap <- leaflet(town) %>% setView(-0, 0, zoom = 10) %>% 
+  addPolygons(stroke=TRUE, smoothFactor = 0.5, weight=1, color=color_id1,popup=content1, opacity=1, fillColor=~colorQuantile('YlOrRd', town$id)(town$id),fillOpacity=1) %>% 
+  addProviderTiles("Stamen.TonerLite") %>% 
+  addLegend(pal=pal1, values= ~town$id, title = "Zipcode Mean of Real Estate Price")
+
+### BUFFALO ###
+
+town <- merge(dataset1, dataset2, by='zipcode',duplicateGeoms=TRUE)
+bins <- bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf) 
+pal1 <- colorBin("YlOrRd",domain = town$id, bins=bins)
+color_id1 <- pal1(town$id)
+content1 <- paste("Neighborhood:", town$neighbourhood,"<br/>") #Popup content to annotate the neighborhoods. 
+townmap <- leaflet(town) %>% setView(-0, 0, zoom = 10) %>% 
+  addPolygons(stroke=TRUE, smoothFactor = 0.5, weight=1, color=color_id1,popup=content1, opacity=1, fillColor=~colorQuantile('YlOrRd', town$id)(town$id),fillOpacity=1) %>% 
+  addProviderTiles("Stamen.TonerLite") %>% 
+  addLegend(pal=pal1, values= ~town$id, title = "Zipcode Mean of Real Estate Price")
+
+### MOORE, OK ####
+
+town <- merge(dataset1, dataset2, by='zipcode',duplicateGeoms=TRUE)
+bins <- bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf) 
+pal1 <- colorBin("YlOrRd",domain = town$id, bins=bins)
+color_id1 <- pal1(town$id)
+content1 <- paste("Neighborhood:", town$neighbourhood,"<br/>") #Popup content to annotate the neighborhoods. 
+townmap <- leaflet(town) %>% setView(-0, 0, zoom = 10) %>% 
+  addPolygons(stroke=TRUE, smoothFactor = 0.5, weight=1, color=color_id1,popup=content1, opacity=1, fillColor=~colorQuantile('YlOrRd', town$id)(town$id),fillOpacity=1) %>% 
+  addProviderTiles("Stamen.TonerLite") %>% 
+  addLegend(pal=pal1, values= ~town$id, title = "Zipcode Mean of Real Estate Price")
+
+### GRAND ISLE ###
+town <- merge(dataset1, dataset2, by='zipcode',duplicateGeoms=TRUE)
+bins <- bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf) 
+pal1 <- colorBin("YlOrRd",domain = town$id, bins=bins)
+color_id1 <- pal1(town$id)
+content1 <- paste("Neighborhood:", town$neighbourhood,"<br/>") #Popup content to annotate the neighborhoods. 
+townmap <- leaflet(town) %>% setView(-0, 0, zoom = 10) %>% 
+  addPolygons(stroke=TRUE, smoothFactor = 0.5, weight=1, color=color_id1,popup=content1, opacity=1, fillColor=~colorQuantile('YlOrRd', town$id)(town$id),fillOpacity=1) %>% 
+  addProviderTiles("Stamen.TonerLite") %>% 
+  addLegend(pal=pal1, values= ~town$id, title = "Zipcode Mean of Real Estate Price")
+
+}
--- Shiny Draft/ui.R
+++ Shiny Draft/ui.R
@@ -11,12 +11,15 @@ vars <- c(
 )
 
 
-navbarPage("Real Estate & Natural Disasters", id="nav",
+navbarPage(h5("Real Estate & Natural Disasters"), id="nav",
            
            ### HOME PAGE ###
            
-           tabPanel("Home Page",
-                    h1("Real Estate amid Disaster: A Data Visualization Exploration"),
+           tabPanel(h6("Home Page"),
+                    
+                    h2("Real Estate amid Disaster: A Data Visualization Exploration"),
+                    h1(" "),
+                    h4("Rory Butler, Cindy Chen, Lizabeth Singh, Jeffray Tsai"),
                     h1(" "),
                     h5("As climate change increases both the intensity and frequency of natural disasters, these consequences also have
                        immediate impacts on real estate, be that housing sale prices, rental prices, or home price indices.  In profiling
@@ -26,90 +29,165 @@ navbarPage("Real Estate & Natural Disasters", id="nav",
                        Is the housing market more resilient to certain types of disasters?  How quickly does it take to bounce back?
                        Our project explores these questions in an engaging manner."),
                     h1(" "),
-                    img(src = "hurricane-katrina.jpg", align = "center"),
-                    img(src='coffey-park-fire.jpg', align = "center"),
-                    img(src='boulder-floods.jpg', align = "center"),
-                    img(src='buffalo-snow.jpg', align = "center"),
-                    img(src='moore-ok-tornado.jpg', align = "center"),
-                    img(src='grand-isle-oil.jpg', align = "center")
+                    img(src = "hurricane-katrina.jpg", align = "center", height = "15%", width = "15%"),
+                    img(src = 'coffey-park-fire.jpg', align = "center", height = "15%", width = "15%"),
+                    img(src = 'boulder-floods.jpg', align = "center", height = "15%", width = "15%"),
+                    img(src = 'buffalo-snow.jpg', align = "center", height = "15%", width = "15%"),
+                    img(src = 'moore-ok-tornado.jpg', align = "center", height = "15%", width = "15%"),
+                    img(src = 'grand-isle-oil.jpg', align = "center", height = "15%", width = "15%")
            ),
            
            ### NEW ORLEANS ###
-           tabPanel("New Orleans, LA - Hurricane",
-                    town <- merge(dataset1, dataset2, by='zipcode',duplicateGeoms=TRUE)
-                    bins <- bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf) 
-                    pal1 <- colorBin("YlOrRd",domain = town$id, bins=bins)
-                    color_id1 <- pal1(town$id)
-                    content1 <- paste("Neighborhood:", town$neighbourhood,"<br/>") #Popup content to annotate the neighborhoods. 
-                    townmap <- leaflet(town) %>% setView(-0, 0, zoom = 10) %>% 
-                      addPolygons(stroke=TRUE, smoothFactor = 0.5, weight=1, color=color_id1,popup=content1, opacity=1, fillColor=~colorQuantile('YlOrRd', town$id)(town$id),fillOpacity=1) %>% 
-                      addProviderTiles("Stamen.TonerLite") %>% 
-                      addLegend(pal=pal1, values= ~town$id, title = "Zipcode Mean of Real Estate Price"))
-          ,
+           tabPanel(h6("New Orleans, LA - Hurricane"),
+                    
+                    tags$head(
+                      includeCSS("styles.css")),
+                    
+                    leaflet::leafletOutput("new_orleans_map"),
+                    
+                    absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                                  draggable = TRUE, top = 170, left = "auto", right = 20, bottom = "auto",
+                                  width = 330, height = "auto",
+                                  
+                                  h4("Hurricane"),
+                                  h4("New Orleans"),
+                                  h6("Aug 2005"),
+                      
+                                  selectInput("color", "Color", vars),
+                                  selectInput("size", "Size", vars, selected = "adultpop"),
+                                  conditionalPanel("input.color == 'superzip' || input.size == 'superzip'",
+                                                   # Only prompt for threshold when coloring or sizing by superzip
+                                                   numericInput("threshold", "SuperZIP threshold (top n percentile)", 5)
+                                  ),
+                                              
+                      plotly::plotlyOutput("bar_chart"),
+                      plotly::plotlyOutput("line_chart"))),
            
            ### COFFEY PARK ###
-           tabPanel("Coffey Park, CA - Wildfires",
-                    town <- merge(dataset1, dataset2, by='zipcode',duplicateGeoms=TRUE)
-                    bins <- bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf) 
-                    pal1 <- colorBin("YlOrRd",domain = town$id, bins=bins)
-                    color_id1 <- pal1(town$id)
-                    content1 <- paste("Neighborhood:", town$neighbourhood,"<br/>") #Popup content to annotate the neighborhoods. 
-                    townmap <- leaflet(town) %>% setView(-0, 0, zoom = 10) %>% 
-                      addPolygons(stroke=TRUE, smoothFactor = 0.5, weight=1, color=color_id1,popup=content1, opacity=1, fillColor=~colorQuantile('YlOrRd', town$id)(town$id),fillOpacity=1) %>% 
-                      addProviderTiles("Stamen.TonerLite") %>% 
-                      addLegend(pal=pal1, values= ~town$id, title = "Zipcode Mean of Real Estate Price"))
-           ,
+           tabPanel(h6("Coffey Park, CA - Wildfires"),
+                    
+                    tags$head(
+                      includeCSS("styles.css")),
+                    
+                    leaflet::leafletOutput("coffey_park_map"),
+                    
+                    absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                                  draggable = TRUE, top = 170, left = "auto", right = 20, bottom = "auto",
+                                  width = 330, height = "auto",
+                                  
+                                  h4("Wildfires"),
+                                  h4("Coffey Park, CA"),
+                                  h6("Oct 2017"),
+                                  
+                                  selectInput("color", "Color", vars),
+                                  selectInput("size", "Size", vars, selected = "adultpop"),
+                                  conditionalPanel("input.color == 'superzip' || input.size == 'superzip'",
+                                                   # Only prompt for threshold when coloring or sizing by superzip
+                                                   numericInput("threshold", "SuperZIP threshold (top n percentile)", 5)
+                                  ),
+                                  
+                                  plotly::plotlyOutput("bar_chart"),
+                                  plotly::plotlyOutput("line_chart"))),
            
-           ### DENVER ###
-           tabPanel("Boulder, CO - Floods", plotOutput("plot")
-                    town <- merge(dataset1, dataset2, by='zipcode',duplicateGeoms=TRUE)
-                    bins <- bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf) 
-                    pal1 <- colorBin("YlOrRd",domain = town$id, bins=bins)
-                    color_id1 <- pal1(town$id)
-                    content1 <- paste("Neighborhood:", town$neighbourhood,"<br/>") #Popup content to annotate the neighborhoods. 
-                    townmap <- leaflet(town) %>% setView(-0, 0, zoom = 10) %>% 
-                      addPolygons(stroke=TRUE, smoothFactor = 0.5, weight=1, color=color_id1,popup=content1, opacity=1, fillColor=~colorQuantile('YlOrRd', town$id)(town$id),fillOpacity=1) %>% 
-                      addProviderTiles("Stamen.TonerLite") %>% 
-                      addLegend(pal=pal1, values= ~town$id, title = "Zipcode Mean of Real Estate Price"))
-           ,
+           ### BOULDER ###
+           tabPanel(h6("Boulder, CO - Floods"), 
+                    
+                    tags$head(
+                      includeCSS("styles.css")),
+                    
+                    leaflet::leafletOutput("boulder_co_map"),
+                    
+                    absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                                  draggable = TRUE, top = 170, left = "auto", right = 20, bottom = "auto",
+                                  width = 330, height = "auto",
+                                  
+                                  h4("Floods"),
+                                  h4("Boulder, CO"),
+                                  h6("Sep 2013"),
+                                  
+                                  selectInput("color", "Color", vars),
+                                  selectInput("size", "Size", vars, selected = "adultpop"),
+                                  conditionalPanel("input.color == 'superzip' || input.size == 'superzip'",
+                                                   # Only prompt for threshold when coloring or sizing by superzip
+                                                   numericInput("threshold", "SuperZIP threshold (top n percentile)", 5)
+                                  ),
+                                  
+                                  plotly::plotlyOutput("bar_chart"),
+                                  plotly::plotlyOutput("line_chart"))),
+           
            
            ### BUFFALO ###
-           tabPanel("Buffalo, NY - Snowstorm", plotOutput("plot")
-                    town <- merge(dataset1, dataset2, by='zipcode',duplicateGeoms=TRUE)
-                    bins <- bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf) 
-                    pal1 <- colorBin("YlOrRd",domain = town$id, bins=bins)
-                    color_id1 <- pal1(town$id)
-                    content1 <- paste("Neighborhood:", town$neighbourhood,"<br/>") #Popup content to annotate the neighborhoods. 
-                    townmap <- leaflet(town) %>% setView(-0, 0, zoom = 10) %>% 
-                      addPolygons(stroke=TRUE, smoothFactor = 0.5, weight=1, color=color_id1,popup=content1, opacity=1, fillColor=~colorQuantile('YlOrRd', town$id)(town$id),fillOpacity=1) %>% 
-                      addProviderTiles("Stamen.TonerLite") %>% 
-                      addLegend(pal=pal1, values= ~town$id, title = "Zipcode Mean of Real Estate Price"))
-           ,
+           tabPanel(h6("Buffalo, NY - Snowstorm"),
+                    
+                    tags$head(
+                      includeCSS("styles.css")),
+                    
+                    leaflet::leafletOutput("boulder_co_map"),
+                    
+                    absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                                  draggable = TRUE, top = 170, left = "auto", right = 20, bottom = "auto",
+                                  width = 330, height = "auto",
+                                  
+                                  h4("Snowstorm"),
+                                  h4("Buffalo, NY"),
+                                  h6("Nov 2014"),
+                                  
+                                  selectInput("color", "Color", vars),
+                                  selectInput("size", "Size", vars, selected = "adultpop"),
+                                  conditionalPanel("input.color == 'superzip' || input.size == 'superzip'",
+                                                   # Only prompt for threshold when coloring or sizing by superzip
+                                                   numericInput("threshold", "SuperZIP threshold (top n percentile)", 5)
+                                  ),
+                                  
+                                  plotly::plotlyOutput("bar_chart"),
+                                  plotly::plotlyOutput("line_chart"))),
            
            ### MOORE, OK ###
-           tabPanel("Moore, OK - Tornado", plotOutput("plot")
-                    town <- merge(dataset1, dataset2, by='zipcode',duplicateGeoms=TRUE)
-                    bins <- bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf) 
-                    pal1 <- colorBin("YlOrRd",domain = town$id, bins=bins)
-                    color_id1 <- pal1(town$id)
-                    content1 <- paste("Neighborhood:", town$neighbourhood,"<br/>") #Popup content to annotate the neighborhoods. 
-                    townmap <- leaflet(town) %>% setView(-0, 0, zoom = 10) %>% 
-                      addPolygons(stroke=TRUE, smoothFactor = 0.5, weight=1, color=color_id1,popup=content1, opacity=1, fillColor=~colorQuantile('YlOrRd', town$id)(town$id),fillOpacity=1) %>% 
-                      addProviderTiles("Stamen.TonerLite") %>% 
-                      addLegend(pal=pal1, values= ~town$id, title = "Zipcode Mean of Real Estate Price"))
-           ,
+           tabPanel(h6("Moore, OK - Tornado"),
+                    tags$head(
+                      includeCSS("styles.css")),
+                    
+                    leaflet::leafletOutput("boulder_co_map"),
+                    
+                    absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                                  draggable = TRUE, top = 170, left = "auto", right = 20, bottom = "auto",
+                                  width = 330, height = "auto",
+                                  
+                                  h4("Tornado"),
+                                  h4("Moore, OK"),
+                                  h6("May 2013"),
+                                  
+                                  selectInput("color", "Color", vars),
+                                  selectInput("size", "Size", vars, selected = "adultpop"),
+                                  conditionalPanel("input.color == 'superzip' || input.size == 'superzip'",
+                                                   # Only prompt for threshold when coloring or sizing by superzip
+                                                   numericInput("threshold", "SuperZIP threshold (top n percentile)", 5)
+                                  ),
+                                  
+                                  plotly::plotlyOutput("bar_chart"),
+                                  plotly::plotlyOutput("line_chart"))),
            
            ### BP OIL SPILL ###
-           tabPanel("Grand Isle, LA - BP Oil Spill", plotOutput("plot")
-                    town <- merge(dataset1, dataset2, by='zipcode',duplicateGeoms=TRUE)
-                    bins <- bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf) 
-                    pal1 <- colorBin("YlOrRd",domain = town$id, bins=bins)
-                    color_id1 <- pal1(town$id)
-                    content1 <- paste("Neighborhood:", town$neighbourhood,"<br/>") #Popup content to annotate the neighborhoods. 
-                    townmap <- leaflet(town) %>% setView(-0, 0, zoom = 10) %>% 
-                      addPolygons(stroke=TRUE, smoothFactor = 0.5, weight=1, color=color_id1,popup=content1, opacity=1, fillColor=~colorQuantile('YlOrRd', town$id)(town$id),fillOpacity=1) %>% 
-                      addProviderTiles("Stamen.TonerLite") %>% 
-                      addLegend(pal=pal1, values= ~town$id, title = "Zipcode Mean of Real Estate Price"))
-           ,
-           
-           conditionalPanel("false", icon("crosshair")))
+           tabPanel(h6("Grand Isle, LA - BP Oil Spill"),
+                    tags$head(
+                      includeCSS("styles.css")),
+                    
+                    leaflet::leafletOutput("boulder_co_map"),
+                    
+                    absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                                  draggable = TRUE, top = 170, left = "auto", right = 20, bottom = "auto",
+                                  width = 330, height = "auto",
+                                  
+                                  h4("Oil Spill"),
+                                  h4("Grand Isle, LA"),
+                                  h6("Apr 2010"),
+                                  
+                                  selectInput("color", "Color", vars),
+                                  selectInput("size", "Size", vars, selected = "adultpop"),
+                                  conditionalPanel("input.color == 'superzip' || input.size == 'superzip'",
+                                                   # Only prompt for threshold when coloring or sizing by superzip
+                                                   numericInput("threshold", "SuperZIP threshold (top n percentile)", 5)
+                                  ),
+                                  
+                                  plotly::plotlyOutput("bar_chart"),
+                                  plotly::plotlyOutput("line_chart"))))
