7bac17aa61875aaed03410522ad9ce275b5fcf13
Cindy Chen <chencindyj@gmail.com>
2022-04-25 21:51:10 -0400

    Add slider

--- Shiny Draft/server.R
+++ Shiny Draft/server.R
@@ -1,89 +1,103 @@
-library(leaflet)
-library(RColorBrewer)
-library(scales)
-library(dplyr)
+
+
 
 # Leaflet bindings are a bit slow; for now we'll just sample to compensate
 set.seed(100)
-dataset1 <- read.csv("~\..\data\buffalo.csv")
-dataset2 <- read.csv("~\..\data\buffalo.csv")
 
 function(input, output, session){
   
+  date_neworleans <- reactive({input$choose_month_neworleans})
+  date_coffeypark <- reactive({input$choose_month_coffey})
+  date_mooreok <- reactive({input$choose_month_moore})
+  date_buffalo <- reactive({input$choose_month_buffalo})
+  date_boulder <- reactive({input$choose_month_boulder})
+  date_grandisle <- reactive({input$choose_month_grandisle})
+  
   ### NEW ORLEANS ###
   
-  town <- merge(dataset1, dataset2, by='zipcode',duplicateGeoms=TRUE)
   
-  bins <- bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf) 
-
-  pal1 <- colorBin("YlOrRd",domain = town$id, bins=bins)
-  
-  color_id1 <- pal1(town$id)
-  
-  content1 <- paste("Neighborhood:", town$neighbourhood,"<br/>") #Popup content to annotate the neighborhoods. 
-  
-  townmap <- leaflet(town) %>% setView(-0, 0, zoom = 10) %>% 
-    addPolygons(stroke=TRUE, smoothFactor = 0.5, weight=1, color=color_id1,popup=content1, opacity=1, fillColor=~colorQuantile('YlOrRd', town$id)(town$id),fillOpacity=1) %>% 
-    addProviderTiles("Stamen.TonerLite") %>% 
-    addLegend(pal=pal1, values= ~town$id, title = "Zipcode Mean of Real Estate Price")
-
   ### COFFEY PARK ###
 
-town <- merge(dataset1, dataset2, by='zipcode',duplicateGeoms=TRUE)
-bins <- bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf) 
-pal1 <- colorBin("YlOrRd",domain = town$id, bins=bins)
-color_id1 <- pal1(town$id)
-content1 <- paste("Neighborhood:", town$neighbourhood,"<br/>") #Popup content to annotate the neighborhoods. 
-townmap <- leaflet(town) %>% setView(-0, 0, zoom = 10) %>% 
-  addPolygons(stroke=TRUE, smoothFactor = 0.5, weight=1, color=color_id1,popup=content1, opacity=1, fillColor=~colorQuantile('YlOrRd', town$id)(town$id),fillOpacity=1) %>% 
-  addProviderTiles("Stamen.TonerLite") %>% 
-  addLegend(pal=pal1, values= ~town$id, title = "Zipcode Mean of Real Estate Price")
+  # load data
+  cali_rent <- read.csv("../data/single_family_homes_time_series.csv")
+  cali_shape <- readOGR("../data/shape files", "California_Zip_Codes")
+  
+  # transform data - rent data
+  cali_rent <- cali_rent %>%
+    select(region_id, date, value) %>%
+    mutate(date = as.Date(date, format = "%Y-%m-%d"))
+  
+  colnames(cali_rent) <- c("zip_code", "date", "median_sale_price")
+  
+  # transform data - shape file
+  cali_shape <- cali_shape[,-c(1,4,5,6,7,8,9)]
+  
+  new_cali_shape <- cali_shape %>%
+    rename(zip_code = ZIP_CODE, po_name = PO_NAME) %>%
+    spTransform(CRS("+proj=longlat +datum=WGS84 +no_defs"))
+  
+  # merge data sources
+  cali_data <- merge(new_cali_shape, cali_rent, by = "zip_code", duplicateGeoms = T)
+  
+  new_cali_data_2 <- cali_data %>%
+    filter(zip_code %in% c(95401, 95402, 95403, 95404, 95405, 95406, 95407, 95409)) %>%
+    st_as_sf(cali_data)
+  
+  bins <-c(100000, 200000, 300000, 400000, 500000)
+  pal <-colorBin("YlOrRd", domain=cali_data_data_26$median_sale_price, bins=bins)
+  
+  output$coffey_park_map <- leaflet::renderLeaflet({
+    new_cali_data_2 %>%
+      filter(date == input$choose_month_coffey) %>%
+      leaflet() %>%
+      addProviderTiles(providers$CartoDB.Positron) %>%
+      addPolygons(
+        fillColor = ~pal(median_sale_price),
+        weight = 2,
+        opacity = 1,
+        color = "gray",
+        fillOpacity = 0.8,
+        highlightOptions = highlightOptions(
+          weight = 5,
+          color = "#666",
+          fillOpacity = 0.8),
+        label = labels,
+        labelOptions = labelOptions(style = list("font-weight" = "normal", padding = "3px 8px"),
+                                    textsize = "15px",
+                                    direction = "auto")) %>%
+      addLegend(pal=pal, values = ~median_sale_price, opacity = 0.8, title = "Mean Rent")})
+  
+  labels <- sprintf(
+    "<strong>%s</strong><br/>Mean Rent: %g",
+    cali_data$zip_code, cali_data_sep_2016$median_salce_price
+  ) %>% lapply(htmltools::HTML)
+  
 
-### DENVER ###
+  ### DENVER ###
 
-town <- merge(dataset1, dataset2, by='zipcode',duplicateGeoms=TRUE)
-bins <- bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf) 
-pal1 <- colorBin("YlOrRd",domain = town$id, bins=bins)
-color_id1 <- pal1(town$id)
-content1 <- paste("Neighborhood:", town$neighbourhood,"<br/>") #Popup content to annotate the neighborhoods. 
-townmap <- leaflet(town) %>% setView(-0, 0, zoom = 10) %>% 
-  addPolygons(stroke=TRUE, smoothFactor = 0.5, weight=1, color=color_id1,popup=content1, opacity=1, fillColor=~colorQuantile('YlOrRd', town$id)(town$id),fillOpacity=1) %>% 
-  addProviderTiles("Stamen.TonerLite") %>% 
-  addLegend(pal=pal1, values= ~town$id, title = "Zipcode Mean of Real Estate Price")
 
-### BUFFALO ###
+  ### BUFFALO ###
 
-town <- merge(dataset1, dataset2, by='zipcode',duplicateGeoms=TRUE)
-bins <- bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf) 
-pal1 <- colorBin("YlOrRd",domain = town$id, bins=bins)
-color_id1 <- pal1(town$id)
-content1 <- paste("Neighborhood:", town$neighbourhood,"<br/>") #Popup content to annotate the neighborhoods. 
-townmap <- leaflet(town) %>% setView(-0, 0, zoom = 10) %>% 
-  addPolygons(stroke=TRUE, smoothFactor = 0.5, weight=1, color=color_id1,popup=content1, opacity=1, fillColor=~colorQuantile('YlOrRd', town$id)(town$id),fillOpacity=1) %>% 
-  addProviderTiles("Stamen.TonerLite") %>% 
-  addLegend(pal=pal1, values= ~town$id, title = "Zipcode Mean of Real Estate Price")
 
-### MOORE, OK ####
+  ### MOORE, OK ####
+  moore_ok <- base_real_estate %>%
+    filter(zip_code %in% c(73160, 73165, 73170),  year %in% c(2011, 2012, 2013, 2014, 2015))
+  
+  moore_ok_shape <- readOGR(dsn="./data/shape files", layer="Moore_OK")
+  
+  moore_ok_shape <- moore_ok_shape[,-c(2,3,4,5,6,7,8,9)]
+  
+  moore_ok_shape2 <- moore_ok_shape %>%
+    rename(zip_code = ZCTA5CE10) %>%
+    spTransform(CRS("+proj=longlat +datum=WGS84 +no_defs"))
+  
+  # merge data
+  moore_ok_data2 <- merge(moore_ok_shape2, moore_ok, by = "zip_code", duplicateGeoms = T) %>%
+    st_as_sf()
 
-town <- merge(dataset1, dataset2, by='zipcode',duplicateGeoms=TRUE)
-bins <- bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf) 
-pal1 <- colorBin("YlOrRd",domain = town$id, bins=bins)
-color_id1 <- pal1(town$id)
-content1 <- paste("Neighborhood:", town$neighbourhood,"<br/>") #Popup content to annotate the neighborhoods. 
-townmap <- leaflet(town) %>% setView(-0, 0, zoom = 10) %>% 
-  addPolygons(stroke=TRUE, smoothFactor = 0.5, weight=1, color=color_id1,popup=content1, opacity=1, fillColor=~colorQuantile('YlOrRd', town$id)(town$id),fillOpacity=1) %>% 
-  addProviderTiles("Stamen.TonerLite") %>% 
-  addLegend(pal=pal1, values= ~town$id, title = "Zipcode Mean of Real Estate Price")
+  st_write(moore_ok_data2, './data/shape files/Moore_Tornado.shp')
 
-### GRAND ISLE ###
-town <- merge(dataset1, dataset2, by='zipcode',duplicateGeoms=TRUE)
-bins <- bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf) 
-pal1 <- colorBin("YlOrRd",domain = town$id, bins=bins)
-color_id1 <- pal1(town$id)
-content1 <- paste("Neighborhood:", town$neighbourhood,"<br/>") #Popup content to annotate the neighborhoods. 
-townmap <- leaflet(town) %>% setView(-0, 0, zoom = 10) %>% 
-  addPolygons(stroke=TRUE, smoothFactor = 0.5, weight=1, color=color_id1,popup=content1, opacity=1, fillColor=~colorQuantile('YlOrRd', town$id)(town$id),fillOpacity=1) %>% 
-  addProviderTiles("Stamen.TonerLite") %>% 
-  addLegend(pal=pal1, values= ~town$id, title = "Zipcode Mean of Real Estate Price")
+
+  ### GRAND ISLE ###
 
 }
--- Shiny Draft/ui.R
+++ Shiny Draft/ui.R
@@ -11,7 +11,7 @@ vars <- c(
 )
 
 
-navbarPage(h5("Real Estate & Natural Disasters"), id="nav",
+navbarPage(h4("Real Estate & Natural Disasters"), id="nav",
            
            ### HOME PAGE ###
            
@@ -53,15 +53,22 @@ navbarPage(h5("Real Estate & Natural Disasters"), id="nav",
                                   h4("New Orleans"),
                                   h6("Aug 2005"),
                       
-                                  selectInput("color", "Color", vars),
+                                  sliderInput(
+                                    inputId = "choose_month_neworleans", 
+                                    label = "Choose the month/year",
+                                    min = as.Date("2002-01-01"),
+                                    max =as.Date("2008-01-01"),
+                                    value= as.Date("2005-08-01"),
+                                    timeFormat="%b %Y"),
+                    
                                   selectInput("size", "Size", vars, selected = "adultpop"),
                                   conditionalPanel("input.color == 'superzip' || input.size == 'superzip'",
                                                    # Only prompt for threshold when coloring or sizing by superzip
                                                    numericInput("threshold", "SuperZIP threshold (top n percentile)", 5)
                                   ),
                                               
-                      plotly::plotlyOutput("bar_chart"),
-                      plotly::plotlyOutput("line_chart"))),
+                      plotly::plotlyOutput("bar_chart_neworleans"),
+                      plotly::plotlyOutput("line_chart_neworleans"))),
            
            ### COFFEY PARK ###
            tabPanel(h6("Coffey Park, CA - Wildfires"),
@@ -71,23 +78,33 @@ navbarPage(h5("Real Estate & Natural Disasters"), id="nav",
                     
                     leaflet::leafletOutput("coffey_park_map"),
                     
-                    absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-                                  draggable = TRUE, top = 170, left = "auto", right = 20, bottom = "auto",
+                    absolutePanel(id = "controls",
+                                  class = "panel panel-default",
+                                  fixed = TRUE,
+                                  draggable = TRUE,
+                                  top = 170, left = "auto", right = 20, bottom = "auto",
                                   width = 330, height = "auto",
                                   
                                   h4("Wildfires"),
                                   h4("Coffey Park, CA"),
                                   h6("Oct 2017"),
                                   
-                                  selectInput("color", "Color", vars),
+                                  sliderInput(
+                                    inputId = "choose_month_coffey", 
+                                    label = "Choose the month/year",
+                                    min = as.Date("2015-01-01"),
+                                    max =as.Date("2020-01-01"),
+                                    value= as.Date("2017-10-01"),
+                                    timeFormat="%b %Y"),
+                                  
                                   selectInput("size", "Size", vars, selected = "adultpop"),
                                   conditionalPanel("input.color == 'superzip' || input.size == 'superzip'",
                                                    # Only prompt for threshold when coloring or sizing by superzip
                                                    numericInput("threshold", "SuperZIP threshold (top n percentile)", 5)
                                   ),
                                   
-                                  plotly::plotlyOutput("bar_chart"),
-                                  plotly::plotlyOutput("line_chart"))),
+                                  plotly::plotlyOutput("bar_chart_coffey"),
+                                  plotly::plotlyOutput("line_chart_coffey"))),
            
            ### BOULDER ###
            tabPanel(h6("Boulder, CO - Floods"), 
@@ -97,23 +114,33 @@ navbarPage(h5("Real Estate & Natural Disasters"), id="nav",
                     
                     leaflet::leafletOutput("boulder_co_map"),
                     
-                    absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-                                  draggable = TRUE, top = 170, left = "auto", right = 20, bottom = "auto",
+                    absolutePanel(id = "controls",
+                                  class = "panel panel-default",
+                                  fixed = TRUE,
+                                  draggable = TRUE,
+                                  top = 170, left = "auto", right = 20, bottom = "auto",
                                   width = 330, height = "auto",
                                   
                                   h4("Floods"),
                                   h4("Boulder, CO"),
                                   h6("Sep 2013"),
                                   
-                                  selectInput("color", "Color", vars),
+                                  sliderInput(
+                                    inputId = "choose_month_boulder", 
+                                    label = "Choose the month/year",
+                                    min = as.Date("2011-01-01"),
+                                    max =as.Date("2016-01-01"),
+                                    value= as.Date("2013-09-01"),
+                                    timeFormat="%b %Y"),
+                                  
                                   selectInput("size", "Size", vars, selected = "adultpop"),
                                   conditionalPanel("input.color == 'superzip' || input.size == 'superzip'",
                                                    # Only prompt for threshold when coloring or sizing by superzip
                                                    numericInput("threshold", "SuperZIP threshold (top n percentile)", 5)
                                   ),
                                   
-                                  plotly::plotlyOutput("bar_chart"),
-                                  plotly::plotlyOutput("line_chart"))),
+                                  plotly::plotlyOutput("bar_chart_boulder"),
+                                  plotly::plotlyOutput("line_chart_boulder"))),
            
            
            ### BUFFALO ###
@@ -124,23 +151,33 @@ navbarPage(h5("Real Estate & Natural Disasters"), id="nav",
                     
                     leaflet::leafletOutput("boulder_co_map"),
                     
-                    absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-                                  draggable = TRUE, top = 170, left = "auto", right = 20, bottom = "auto",
+                    absolutePanel(id = "controls",
+                                  class = "panel panel-default",
+                                  fixed = TRUE,
+                                  draggable = TRUE,
+                                  top = 170, left = "auto", right = 20, bottom = "auto",
                                   width = 330, height = "auto",
                                   
                                   h4("Snowstorm"),
                                   h4("Buffalo, NY"),
                                   h6("Nov 2014"),
                                   
-                                  selectInput("color", "Color", vars),
+                                  sliderInput(
+                                    inputId = "choose_month_buffalo", 
+                                    label = "Choose the month/year",
+                                    min = as.Date("2012-01-01"),
+                                    max =as.Date("2017-01-01"),
+                                    value= as.Date("2014-11-01"),
+                                    timeFormat="%b %Y"),
+                                  
                                   selectInput("size", "Size", vars, selected = "adultpop"),
                                   conditionalPanel("input.color == 'superzip' || input.size == 'superzip'",
                                                    # Only prompt for threshold when coloring or sizing by superzip
                                                    numericInput("threshold", "SuperZIP threshold (top n percentile)", 5)
                                   ),
                                   
-                                  plotly::plotlyOutput("bar_chart"),
-                                  plotly::plotlyOutput("line_chart"))),
+                                  plotly::plotlyOutput("bar_chart_buffalo"),
+                                  plotly::plotlyOutput("line_chart_buffalo"))),
            
            ### MOORE, OK ###
            tabPanel(h6("Moore, OK - Tornado"),
@@ -149,45 +186,65 @@ navbarPage(h5("Real Estate & Natural Disasters"), id="nav",
                     
                     leaflet::leafletOutput("boulder_co_map"),
                     
-                    absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-                                  draggable = TRUE, top = 170, left = "auto", right = 20, bottom = "auto",
+                    absolutePanel(id = "controls",
+                                  class = "panel panel-default",
+                                  fixed = TRUE,
+                                  draggable = TRUE,
+                                  top = 170, left = "auto", right = 20, bottom = "auto",
                                   width = 330, height = "auto",
                                   
                                   h4("Tornado"),
                                   h4("Moore, OK"),
                                   h6("May 2013"),
                                   
-                                  selectInput("color", "Color", vars),
+                                  sliderInput(
+                                    inputId = "choose_month_moore", 
+                                    label = "Choose the month/year",
+                                    min = as.Date("2011-01-01"),
+                                    max =as.Date("2016-01-01"),
+                                    value= as.Date("2013-05-01"),
+                                    timeFormat="%b %Y"),
+                                  
                                   selectInput("size", "Size", vars, selected = "adultpop"),
                                   conditionalPanel("input.color == 'superzip' || input.size == 'superzip'",
                                                    # Only prompt for threshold when coloring or sizing by superzip
                                                    numericInput("threshold", "SuperZIP threshold (top n percentile)", 5)
                                   ),
                                   
-                                  plotly::plotlyOutput("bar_chart"),
-                                  plotly::plotlyOutput("line_chart"))),
+                                  plotly::plotlyOutput("bar_chart_moore"),
+                                  plotly::plotlyOutput("line_chart_moore"))),
            
            ### BP OIL SPILL ###
            tabPanel(h6("Grand Isle, LA - BP Oil Spill"),
                     tags$head(
                       includeCSS("styles.css")),
                     
-                    leaflet::leafletOutput("boulder_co_map"),
+                    leaflet::leafletOutput("grand_isle_map"),
                     
-                    absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-                                  draggable = TRUE, top = 170, left = "auto", right = 20, bottom = "auto",
+                    absolutePanel(id = "controls",
+                                  class = "panel panel-default",
+                                  fixed = TRUE,
+                                  draggable = TRUE,
+                                  top = 170, left = "auto", right = 20, bottom = "auto",
                                   width = 330, height = "auto",
                                   
                                   h4("Oil Spill"),
                                   h4("Grand Isle, LA"),
                                   h6("Apr 2010"),
                                   
-                                  selectInput("color", "Color", vars),
+                                  sliderInput(
+                                    inputId = "choose_month_grandisle", 
+                                    label = "Choose the month/year",
+                                    min = as.Date("2007-01-01"),
+                                    max =as.Date("2013-01-01"),
+                                    value= as.Date("2010-04-01"),
+                                    timeFormat="%b %Y"),
+                                  
                                   selectInput("size", "Size", vars, selected = "adultpop"),
                                   conditionalPanel("input.color == 'superzip' || input.size == 'superzip'",
                                                    # Only prompt for threshold when coloring or sizing by superzip
                                                    numericInput("threshold", "SuperZIP threshold (top n percentile)", 5)
                                   ),
                                   
-                                  plotly::plotlyOutput("bar_chart"),
-                                  plotly::plotlyOutput("line_chart"))))
+                                  plotly::plotlyOutput("bar_chart_grandisle"),
+                                  plotly::plotlyOutput("line_chart_grandisle"))))
